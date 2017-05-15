16612226a1f0e1e7c25fdddf283e324c21fa645f
Sean Genabe <seangenabe@outlook.com>
2017-05-15 16:59:08 +0800

    convert tests to ava

---
+++ test/index.test.js
@@ -0,0 +1,42 @@
+import join from '..'
+import exec from './exec'
+import test from 'ava'
+
+if (process.platform === 'win32') {
+
+  const commands1 = ['a', 'b.txt', 'c/d', "'e'"]
+  const commands2 = ['a\\b', 'a b', 'a\tb', 'a\\\\\\b', 'a"b"c"d"\\"\\',
+    'a&<>|;', '%c%', '%D']
+
+  test('should join normally', t => {
+    t.is(join(commands1), "a b.txt c/d 'e'")
+  })
+
+  test('should exec normally', t => {
+    t.is(exec(commands1), commands1.join('\n'))
+  })
+
+  test('should exec to escape special characters', t => {
+    t.is(exec(commands2), commands2.join('\n'))
+  })
+
+}
+else {
+
+  const commands1 = ['a', 'b', 'c', 'd.txt']
+  const commands2 = ["'a", "b'c", "d\\e", "f\\\\''g", "h i", "' j '", "k?",
+      "l[", "m]", "`n`", "$o", "$(p)", "#q", "r & s", "t < u", "v > w", "x'"]
+
+  test('should join normally', t => {
+    t.is(join(commands1), 'a b c d.txt')
+  })
+
+  test('should exec normally', t => {
+    t.is(exec(commands1), commands1.join('\n'))
+  })
+
+  test('should exec string with single quotes', t => {
+    t.is(exec(commands2), commands2.join('\n'))
+  })
+
+}
